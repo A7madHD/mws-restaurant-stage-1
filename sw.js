6a64cb5e1d953536054c6eb80fdd451dc28467d8
A7mad <s201548510@kfupm.edu.sa>
2019-07-02 00:19:23 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,37 @@
+var cacheName = "revRest-cahce-v";
+var randomVerNum = Math.floor(random()*10000);
+cacheName += randomVerNum;
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(cacheName).then(function(cahce){
+      console.log(`The Cache ${cacheName} is open!`);
+      return Cache.addAll([
+        './index.html',
+        './restaurant.html',
+        './css/main.css',
+        './css/responsive.css',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './img/*',
+        './data/*'
+      ]);
+    })
+    .catch(function(error) {
+      return 'Opps, there is this error: ' + error;
+      })
+  )
+})
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(clients.claims())
+})
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if (response) return response; return fetch(event.request);
+    })
+  )
+})
